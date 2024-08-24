36e75c1c035f209f1428e1e33205cf48bc480e82
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-08-24 19:59:45 -0300

    refactor(api): add Extesion pycripto and type UUID, alterate data name for date_timerefactor(api): added pycrypto extension and UUID data type, changed date column name to date_time

--- api-server/sql-scripts/init-db.sql
+++ api-server/sql-scripts/init-db.sql
@@ -1,23 +1,25 @@
-CREATE TABLE  "USER" (
-    user_id SERIAL PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE CLIENT (
+    client_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     date_time TIMESTAMP NOT NULL,
     name VARCHAR(40) NOT NULL,
-    email VARCHAR(60) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE  CSV_IMPORT (
-    id SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL,
-    user_id_fk INT NOT NULL,
-    csv_id INT NOT NULL,
+CREATE TABLE CSV_IMPORT (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    date_time TIMESTAMP NOT NULL,
+    client_id_fk UUID NOT NULL,
+    csv_id UUID DEFAULT gen_random_uuid() NOT NULL,
     type_import INT NOT NULL,
-    FOREIGN KEY (user_id_fk) REFERENCES "USER"(user_id)
+    FOREIGN KEY (client_id_fk) REFERENCES CLIENT(client_id)
 );
 
-CREATE TABLE  CSV_DATA (
-    csv_id SERIAL PRIMARY KEY,
-    csv_import_id_fk INT NOT NULL,
+CREATE TABLE CSV_DATA (
+    csv_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    csv_import_id_fk UUID NOT NULL,
     data TIMESTAMP NOT NULL,
     balance REAL NOT NULL,
     equity REAL NOT NULL,
