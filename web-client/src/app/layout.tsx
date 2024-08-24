7e4669a7dc2e5e4fb631f74b05fe93f8990f2964
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-24 18:05:02 -0300

    feat(web): add base layout with side menu and header

--- web-client/src/app/layout.tsx
+++ web-client/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { SideMenu } from '~/components/side-menu'
+import { MainHeader } from '~/components/main-header'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,8 +19,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='pt-br'>
+      <body className={inter.className}>
+        <div className='flex bg-secondary-foreground'>
+          <SideMenu />
+          <div className='flex-1 flex flex-col'>
+            <MainHeader />
+            <div className='flex-1 p-10 border borde-red-600 bg-background rounded-lg'>
+              {children}
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
