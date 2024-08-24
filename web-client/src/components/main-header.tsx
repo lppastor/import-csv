2875c037209175aa1b27e51f80402a3e62a643f2
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-24 18:01:31 -0300

    feat(web): create main-header component

---
+++ web-client/src/components/main-header.tsx
@@ -0,0 +1,7 @@
+export function MainHeader() {
+  return (
+    <header className='bg-secondary-foreground text-secondary w-full px-10 py-3 flex justify-end'>
+      <span>sair</span>
+    </header>
+  )
+}
