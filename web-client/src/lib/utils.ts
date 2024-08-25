11e1b34bcbc5e91eae3c05c7eed01cc5c859b200
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-25 12:36:15 -0300

    feat(web): create `moneyFormat` helper

--- web-client/src/lib/utils.ts
+++ web-client/src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function moneyFormat(value: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value)
 }
