b6ab6bc28b1e60f8d4086f61039adab5a72cb0b0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-26 21:22:25 -0300

    refactor(web): create `import-card` component

--- web-client/src/app/page.tsx
+++ web-client/src/app/page.tsx
@@ -1,13 +1,7 @@
-import { CloudUpload, Dot, Eye, Trash2 } from 'lucide-react'
-import { Date } from '~/components/date'
+import { CloudUpload } from 'lucide-react'
+
+import { Card } from '~/components/import-card'
 import { Button } from '~/components/ui/button'
-import {
-  Card as CardContainer,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '~/components/ui/card'
 import { Input } from '~/components/ui/input'
 import { Label } from '~/components/ui/label'
 import {
@@ -17,78 +11,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '~/components/ui/select'
-import { CSVImportReturn, fetchCSVImports } from '~/lib/fake-data'
-import { moneyFormat } from '~/lib/utils'
-
-const Card = ({ importData }: { importData: CSVImportReturn }) => {
-  const {
-    id,
-    import_type,
-    created_at,
-    balance_sum,
-    data_lines,
-    deposit_sum,
-    equity_sum,
-  } = importData
-
-  const typeString: string = import_type === 'direct' ? 'Direta' : 'Indireta'
-
-  return (
-    <CardContainer className='shadow-lg'>
-      <CardHeader>
-        <CardTitle>Importação {id.toString().padStart(3, '0')}</CardTitle>
-        <CardDescription className='flex items-center gap-px'>
-          <span>{typeString}</span>
-          <Dot />
-          <Date dateString={created_at} />
-        </CardDescription>
-      </CardHeader>
-      <CardContent className='space-y-5'>
-        <div>
-          <div className='flex justify-between items-center'>
-            <span>Balanço total</span>
-            <span className='flex-1 mx-2 border-b border-dotted border-gray-500' />
-            <span className='font-bold'>{moneyFormat(balance_sum)}</span>
-          </div>
-          <div className='flex justify-between items-center'>
-            <span>Total de depósitos</span>
-            <span className='flex-1 mx-2 border-b border-dotted border-gray-500' />
-            <span className='font-bold'>{moneyFormat(deposit_sum)}</span>
-          </div>
-          <div className='flex justify-between items-center'>
-            <span>Equity total</span>
-            <span className='flex-1 mx-2 border-b border-dotted border-gray-500' />
-            <span className='font-bold'>{moneyFormat(equity_sum)}</span>
-          </div>
-          <div className='flex justify-between items-center'>
-            <span>Número de linhas</span>
-            <span className='flex-1 mx-2 border-b border-dotted border-gray-500' />
-            <span className='font-bold'>{data_lines}</span>
-          </div>
-        </div>
-        <div className='flex items-center justify-end gap-3'>
-          <Button
-            variant='ghost'
-            size='sm'
-            className='flex gap-1 items-center hover:text-destructive hover:bg-destructive/10'
-          >
-            <Trash2 size='1.2em' />
-            <span>Deletar</span>
-          </Button>
-
-          <Button
-            size='sm'
-            variant='secondary'
-            className='flex gap-1 items-center'
-          >
-            <Eye size='1.2em' />
-            <span>Visualizar</span>
-          </Button>
-        </div>
-      </CardContent>
-    </CardContainer>
-  )
-}
+import { fetchCSVImports } from '~/lib/fake-data'
 
 export default function Home() {
   const imports = fetchCSVImports()
