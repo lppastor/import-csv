3a96b5cbce8cde8b1d0141d18c5419423ebfa167
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-26 21:03:12 -0300

    refactor(web): create `nav-item` component

---
+++ web-client/src/components/nav-item.tsx
@@ -0,0 +1,94 @@
+import { cva } from 'class-variance-authority'
+import { LucideIcon } from 'lucide-react'
+import Link from 'next/link'
+import { cn } from '~/lib/utils'
+
+type NavItemProps = {
+  Icon: LucideIcon
+  title: string
+  path: string
+  open: boolean
+  active?: boolean
+}
+
+const linkVariants = cva('flex items-center group', {
+  variants: {
+    open: {
+      true: 'gap-1',
+    },
+  },
+})
+
+const leftMarkerVariants = cva('h-7 rounded-full', {
+  variants: {
+    active: {
+      true: 'bg-secondary',
+    },
+    open: {
+      true: 'w-1',
+      false: 'hidden',
+    },
+  },
+})
+
+const itemContentContainerVariants = cva(
+  'flex items-center gap-2 p-1 px-2 rounded-lg w-full group-hover:bg-secondary/20',
+  {
+    variants: {
+      active: {
+        true: 'bg-secondary/10',
+      },
+    },
+  }
+)
+
+const iconVariant = cva('text-secondary/60', {
+  variants: {
+    open: { false: 'text-2xl' },
+    active: { true: '' },
+  },
+  compoundVariants: [
+    {
+      active: true,
+      open: false,
+      class: 'text-secondary',
+    },
+  ],
+})
+
+const labelVariant = cva('text-secondary font-semibold', {
+  variants: {
+    open: {
+      false: 'hidden',
+    },
+  },
+})
+
+export function NavItem({
+  Icon,
+  path,
+  title,
+  open,
+  active = false,
+}: NavItemProps) {
+  return (
+    <Link href={path} className={cn(linkVariants({ open }))}>
+      <div
+        className={cn(
+          leftMarkerVariants({
+            active,
+            open,
+          })
+        )}
+      />
+
+      <div
+        data-active={active}
+        className={cn(itemContentContainerVariants({ active }))}
+      >
+        <Icon size='1em' className={cn(iconVariant({ active, open }))} />
+        <span className={cn(labelVariant({ open }))}>{title}</span>
+      </div>
+    </Link>
+  )
+}
--- web-client/src/components/side-menu.tsx
+++ web-client/src/components/side-menu.tsx
@@ -1,53 +1,10 @@
 'use client'
 
-import Link from 'next/link'
-import { AlignJustify, ChartLine, CloudUpload, LucideIcon } from 'lucide-react'
+import { useState } from 'react'
+import { AlignJustify, ChartLine, CloudUpload } from 'lucide-react'
 
 import { Button } from './ui/button'
-import { useState } from 'react'
-
-type NavItemProps = {
-  Icon: LucideIcon
-  title: string
-  path: string
-  open: boolean
-  active?: boolean
-}
-
-function NavItem({ Icon, path, title, open, active = false }: NavItemProps) {
-  if (open) {
-    return (
-      <Link href={path} className='flex items-center gap-1 group'>
-        <div
-          data-active={active}
-          className='h-7 rounded-full w-1 data-[active="true"]:bg-secondary'
-        />
-        <div
-          data-active={active}
-          className='flex items-center gap-2 data-[active="true"]:bg-secondary/10 p-1 px-2 rounded-lg w-full group-hover:bg-secondary/20 transition-colors'
-        >
-          <Icon size='1em' className='text-secondary/60' />
-          <span className='text-secondary font-semibold'>{title}</span>
-        </div>
-      </Link>
-    )
-  } else {
-    return (
-      <Link href={path} className='group'>
-        <div
-          data-active={active}
-          className='flex items-center gap-2 data-[active="true"]:bg-secondary/10 p-1 px-2 rounded-lg w-full group-hover:bg-secondary/20 transition-colors'
-        >
-          <Icon
-            data-active={active}
-            size='1.5em'
-            className='text-secondary/60 data-[active="true"]:text-secondary'
-          />
-        </div>
-      </Link>
-    )
-  }
-}
+import { NavItem } from './nav-item'
 
 export function SideMenu() {
   const [open, setOpen] = useState(true)
