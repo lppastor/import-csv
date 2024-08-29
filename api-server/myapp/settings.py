5d8c47c5a5dc895f152bf52b5c3ad0b7e55d298a
Lucas Maciel Campos <142741602+LucasMaciel7@users.noreply.github.com>
2024-08-29 08:24:01 -0300

    feat(api): add django_key envroinment variable

--- api-server/myapp/settings.py
+++ api-server/myapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o563-^f8f@z&d(*5ocgt!auz*@ipnc$whe&9)mk1*w!idy$w3u'
+SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
