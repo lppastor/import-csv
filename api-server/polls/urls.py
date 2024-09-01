a06b3166edd8265c064bdbbda2156db9c8454eeb
Lucas Maciel Campos <142741602+LucasMaciel7@users.noreply.github.com>
2024-08-31 22:32:07 -0300

    Clenaup(api): Delete api-server/polls directory

--- api-server/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from  .views import views
-
-urlpatterns = [
-    path("", views.index, name= "index"),
-    path("imports/", views.helloWord, name="members"), ## Return array de ids importado pelo usuarip
-]
