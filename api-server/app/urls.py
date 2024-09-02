0c9329c490407d33b74d91f3599d27eea7601f34
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-02 08:22:21 -0300

    feat(api): Register route user-imports

--- api-server/app/urls.py
+++ api-server/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views.import_data import import_data
-
+from .views.import_data import import_data,get_user_imports
 urlpatterns = [
     path("import-data/", import_data, name="import_data"),
+    path('user-imports/<str:user_id>/', get_user_imports, name='user_imports')
 ]
