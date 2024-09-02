b7c2a4da35b35f9d4ffd708d56ecbbc02103ecd2
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-01 22:05:18 -0300

    feat(api): Register models in admin painel

--- api-server/app/admin.py
+++ api-server/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app.models.client import Client
+from app.models.csv_data import CsvData
+from app.models.csv_import import CsvImport
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(CsvData)
+admin.site.register(CsvImport)
