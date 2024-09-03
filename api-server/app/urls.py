5a84091162bb7ffdaf2253af58d321fead22a9ae
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-03 08:31:09 -0300

    feat(api): Endpoint graph data

--- api-server/app/urls.py
+++ api-server/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views.import_data import import_data,get_user_imports
+from .views.graphdata import get_balance_summary
+
 urlpatterns = [
     path("import-data/", import_data, name="import_data"),
-    path('user-imports/<str:user_id>/', get_user_imports, name='user_imports')
+    path('user-imports/<str:user_id>/', get_user_imports, name='user_imports'),
+    path('graph-data/', get_balance_summary, name='graph-data'),
 ]
