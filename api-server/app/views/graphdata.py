cff0a9cf7f96cd51512d3b74636029ed8f03d7e4
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-05 08:35:52 -0300

    fix(api): Return response datas

--- api-server/app/views/graphdata.py
+++ api-server/app/views/graphdata.py
@@ -4,7 +4,12 @@ from app.repository.csv_data import CsvDataRepository
 
 def get_balance_summary(request):
     client_id = request.GET.get('client_id')
-    import_ids = request.GET.getlist('import_ids[]')
+    import_ids= request.GET.get('import_ids')
+    
+    if not import_ids:
+        return JsonResponse({"error": "Client not found"}, status=400)
+
+    import_ids= import_ids.split(',')
 
     # Obtém o cliente pelo ID usando o ClientRepository
     client = ClientRepository.get_client_by_id(client_id)
@@ -15,6 +20,6 @@ def get_balance_summary(request):
     csv_data = CsvDataRepository.get_csv_data_by_import_ids_and_client(import_ids, client)
 
     # Agrupa por mês e por import_id, somando os valores de balance
-    response_data = CsvDataRepository.get_monthly_balance_summary(csv_data, import_ids)
+    response_data = CsvDataRepository.get_monthly_balance_summary(csv_data)
 
     return JsonResponse(response_data, safe=False, status=200)
