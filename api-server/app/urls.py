f7866bd37a8315081ebd3aca94fe3b65b18fb09f
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-08 17:45:53 -0300

    feat(api): Register router get_csv_data

--- api-server/app/urls.py
+++ api-server/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.import_data import import_data
+from .views.import_data import import_data, get_csv_data
 from .views.get_user_imports import get_user_imports
 from .views.graphdata import get_balance_summary
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("import-data/", import_data, name="import_data"),
     path('user-imports/<str:user_id>/', get_user_imports, name='user_imports'),
     path('graph-data/', get_balance_summary, name='graph-data'),
+    path('csv-data/', get_csv_data, name='csv-data')
 ]
