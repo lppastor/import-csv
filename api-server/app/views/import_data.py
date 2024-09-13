27bb39c06eaf576c06c7d0c3523a6313bb62d814
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-13 07:40:25 -0300

    cleanup(api): Add and swagger auto schema

--- api-server/app/views/import_data.py
+++ api-server/app/views/import_data.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
 from django.http import JsonResponse
-from django.http import HttpResponse
-from django.core.exceptions import BadRequest
 from django.views.decorators.csrf import csrf_exempt
 from ..repository.client import ClientRepository # type: ignore
 from ..repository.csv_data import CsvDataRepository
@@ -10,96 +6,88 @@ import json
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from app.serializers import CsvDataSerializer, ImportDataSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 
 
-# POST CSV DATA R
+
 @csrf_exempt
 @swagger_auto_schema(
     method='post',
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'client_id': openapi.Schema(type=openapi.TYPE_STRING, description='ID do cliente'),
-            'import_type': openapi.Schema(type=openapi.TYPE_STRING, description='Tipo de importação'),
-            'data': openapi.Schema(
-                type=openapi.TYPE_ARRAY,
-                items=openapi.Schema(
-                    type=openapi.TYPE_OBJECT,
-                    properties={
-                        'date': openapi.Schema(type=openapi.TYPE_STRING, description='Data em formato ISO'),
-                        'balance': openapi.Schema(type=openapi.TYPE_NUMBER, description='Saldo'),
-                        'equity': openapi.Schema(type=openapi.TYPE_NUMBER, description='Equity'),
-                        'deposit': openapi.Schema(type=openapi.TYPE_NUMBER, description='Depósito'),
-                    }
-                )
-            ),
-        },
-        required=['client_id', 'import_type', 'data'],
-        example={
-            'client_id': '12345',
-            'import_type': 'tipo_de_importacao',
-            'data': [
-                {
-                    'date': '2024-09-11T00:00:00Z',
-                    'balance': 1000.0,
-                    'equity': 1500.0,
-                    'deposit': 500.0,
-                },
-                {
-                    'date': '2024-09-12T00:00:00Z',
-                    'balance': 2000.0,
-                    'equity': 2500.0,
-                    'deposit': 1000.0,
-                }
-            ]
-        }
-    )
+    request_body=ImportDataSerializer,  # Definindo o corpo da requisição
+    responses={
+        201: openapi.Response('Insert successfully completed'),
+        404: openapi.Response('Client not found'),
+        400: openapi.Response('Unable to insert csv, please check the information and try again.')
+    }
 )
 @api_view(['POST'])
 def import_data(request):
     if request.method == "POST":
         try:
-            #Data Parse
-            body= json.loads(request.body)
-            client_id= body.get('client_id')
-            import_type = body.get('import_type')
-            data_list = body.get("data")
-
-            client = ClientRepository.get_client_by_id(client_id) # Indentifica o Cliente 
+            serializer = ImportDataSerializer(data=request.data)
             
-            if not client:
-                return JsonResponse({
-                    "error":"Client not found"
-                    },status=404
+            if serializer.is_valid():
+                validate_data = serializer.validated_data
+
+                client_id = validate_data['client_id']
+                import_type = validate_data['import_type']
+                data_list = validate_data['data']
+                client = ClientRepository.get_client_by_id(client_id) # Indentifica o Cliente 
+            
+                if not client:
+                    return JsonResponse({
+                        "error":"Client not found"
+                        },status=status.HTTP_400_BAD_REQUEST
+                        )
+            
+            
+                #Insert em csv_import
+                csv_import = CsvDataRepository.create_csv_import(
+                    client= client,
+                    import_type= import_type
+                )
+            
+                # Insert em Csv_data
+                
+                for data in data_list:
+                    CsvDataRepository.create_csv_data(
+                        csv_import= csv_import,
+                        date_time= data['date'],
+                        balance= data['balance'],
+                        equity= data['equity'],
+                        deposit= data['deposit']
+                        
                     )
                 
-            
-            #Insert em csv_import
-            csv_import = CsvDataRepository.create_csv_import(
-                client= client,
-                import_type= import_type
-            )
-            
-            # Insert em Csv_data
-            
-            for data in data_list:
-                CsvDataRepository.create_csv_data(
-                    csv_import= csv_import,
-                    date_time= data['date'],
-                    balance= data['balance'],
-                    equity= data['equity'],
-                    deposit= data['deposit']
-                    
-                )
+                return JsonResponse({
+                    "status":"Insert successfully completed"
+                },status=status.HTTP_201_CREATED) 
                 
-            return JsonResponse({
-                "status":"Insert successfully completed"
-            },status=201)        
+            else:
+                return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         
+      
+
         except Exception as e:
-            return BadRequest('Unable to insert csv, please check the information and try again.')
-        
+            print(f"Erro: {str(e)}")
+            return JsonResponse({
+                "error": "Unable to insert csv, please check the information and try again.",
+                "details": str(e)  
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@swagger_auto_schema(
+    method='get',
+    manual_parameters=[
+        openapi.Parameter('client', openapi.IN_QUERY, description="Client ID", type=openapi.TYPE_STRING),
+        openapi.Parameter('import_id', openapi.IN_QUERY, description="Import ID", type=openapi.TYPE_INTEGER)
+    ]
+)   
 @api_view(['GET'])          
 def get_csv_data(request):
     client_id = request.GET.get('client')
@@ -111,7 +99,7 @@ def get_csv_data(request):
         
        
     if not csv_data.exists(): 
-        return JsonResponse({"status": "CSV data not found."}, status=404)
+        return JsonResponse({"status": "CSV data not found."}, status=status.HTTP_400_BAD_REQUEST)
  
     csv_list = []
 
@@ -126,5 +114,5 @@ def get_csv_data(request):
         })
         
     # Retornar os dados como JSON com status 200
-    return JsonResponse(csv_list, safe=False, status=200)
+    return JsonResponse(csv_list, safe=False, status=status.HTTP_200_OK)
 
