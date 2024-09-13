a4fec2f4cffb5d8f2dcde54b027e62df0e6868c1
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-13 07:41:49 -0300

    Rename descriptions and title for swagger

--- api-server/project/urls.py
+++ api-server/project/urls.py
@@ -8,11 +8,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="API Documentation",
+        title="Import-csv API",
         default_version='v1',
-        description="API documentation with drf-yasg",
+        description="Documentação descritiva de nossos endpoints",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="support@example.com"),
+        contact=openapi.Contact(email="lucasmacielcampos27@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path( 'app/swagger/' , schema_view.with_ui( 'swagger' , cache_timeout= 0 ), name= 'schema-swagger-ui' ), 
-    path( 'redoc/' , schema_view.with_ui( 'redoc' , cache_timeout= 0 ), name= 'schema-redoc' ), 
+    path( 'app/redoc/' , schema_view.with_ui( 'redoc' , cache_timeout= 0 ), name= 'schema-redoc' ), 
     path("app/", include("app.urls")),
     path('admin/', admin.site.urls)
 ]
