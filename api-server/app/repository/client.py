63b52d109c8fb5fd4ebacb9fd7d16508a8afb406
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 14:55:40 -0300

    feat(api): Create ClientRepository an create client

--- api-server/app/repository/client.py
+++ api-server/app/repository/client.py
@@ -1,18 +1,33 @@
 from django.db import models
 from ..models.client import Client
 from django.contrib.auth.hashers import make_password  # Create Hash passworf PBKDF2 com Salt
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
+Client = get_user_model() # Busca a model personalizada de Client
 
 class ClientRepository:
     @classmethod
-    def create_user(name,email,password):
-        passwd_hash = make_password(password) # Generate Hash
-        user = Client(
-        name = name,
-        email = email,
-        password = passwd_hash # Insert Hash
+    def create_user(cls, first_name, last_name, email, password, username = None, is_superuser = False, is_staff = False, is_active = True):
+        if Client.objects.filter(email=email).exists():
+            raise ValidationError("Um usuario com este email ja existe")      
+        
+        hash_passwd = make_password(password)
+        
+        user = Client.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = hash_passwd,
+            username = email,
+            is_superuser = is_superuser,
+            is_staff = is_staff,
+            is_active = is_active,
+            date_joined  = datetime.now(),   
         )
-        user.save()
+        return user
+        
         
     @classmethod    
     def get_client_by_id(cls, client_id):
