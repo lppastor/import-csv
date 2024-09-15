72940dd3241df7017593f0dfbeda4ff69c7ece05
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 12:13:35 -0300

    fix(api): This username is required for create superuser

--- api-server/app/models/client.py
+++ api-server/app/models/client.py
@@ -6,7 +6,7 @@ class Client(AbstractUser):
     client_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username'] 
     
     def __str__(self):
         return self.email
