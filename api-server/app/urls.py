24f1dfe39eff2417b61fb2512d191802479d3aea
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 20:30:49 -0300

    feat(api): Register  login router

--- api-server/app/urls.py
+++ api-server/app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from .views.import_data import import_data, get_csv_data
 from .views.get_user_imports import get_user_imports
 from .views.graphdata import get_balance_summary
-from .views.Client import Create_client
+from .views.Client import Create_client, ClientLogin
 
 urlpatterns = [
     path("import-data/", import_data, name="import_data"),
     path('user-imports/<str:user_id>/', get_user_imports, name='user_imports'),
     path('graph-data/', get_balance_summary, name='graph-data'),
     path('csv-data/', get_csv_data, name='csv-data'),
-    path('client/register_client/', Create_client, name="Register Client")
+    path('client/register_client/', Create_client, name="Register Client"),
+    path('client/login/', ClientLogin, name= "Login")
 ]
