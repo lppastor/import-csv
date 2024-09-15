f0a7b248e45cea632497743a1731f62f6b1fe9f3
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 12:14:29 -0300

    fix(api): app.Client foreinkey

--- api-server/app/models/csv_import.py
+++ api-server/app/models/csv_import.py
@@ -1,12 +1,11 @@
 from django.db import models
-from .client import Client
 
 class CsvImport(models.Model):
     import_id = models.AutoField(primary_key=True)
     csv_import_date = models.DateTimeField(auto_now_add=True)
     type_import = models.IntegerField()
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey('app.Client', to_field="client_id", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Import {self.import_id} for {self.client.name}"
+        return f"Import {self.import_id} for {self.client}"
