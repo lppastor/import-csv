ee2682af7007737313e53383968f39716fca7bdd
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 14:57:43 -0300

    Register router url register_client

--- api-server/app/urls.py
+++ api-server/app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views.import_data import import_data, get_csv_data
 from .views.get_user_imports import get_user_imports
 from .views.graphdata import get_balance_summary
+from .views.Client import Create_client
 
 urlpatterns = [
     path("import-data/", import_data, name="import_data"),
     path('user-imports/<str:user_id>/', get_user_imports, name='user_imports'),
     path('graph-data/', get_balance_summary, name='graph-data'),
-    path('csv-data/', get_csv_data, name='csv-data')
+    path('csv-data/', get_csv_data, name='csv-data'),
+    path('register_client', Create_client, name="Register Client")
 ]
