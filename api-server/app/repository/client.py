49041810da7fdff61096ab2f45195e6138db17a4
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 20:29:23 -0300

    feat(api): Create login method

--- api-server/app/repository/client.py
+++ api-server/app/repository/client.py
@@ -2,6 +2,10 @@ from django.db import models
 from ..models.client import Client
 from django.contrib.auth.hashers import make_password  # Create Hash passworf PBKDF2 com Salt
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 from django.core.exceptions import ValidationError
 from datetime import datetime
 
@@ -13,13 +17,13 @@ class ClientRepository:
         if Client.objects.filter(email=email).exists():
             raise ValidationError("Um usuario com este email ja existe")      
         
-        hash_passwd = make_password(password)
         
-        user = Client.objects.create(
+        
+        user = Client.objects.create_user(
             first_name = first_name,
             last_name = last_name,
             email = email,
-            password = hash_passwd,
+            password = password,
             username = email,
             is_superuser = is_superuser,
             is_staff = is_staff,
@@ -27,6 +31,25 @@ class ClientRepository:
             date_joined  = datetime.now(),   
         )
         return user
+    
+    
+    @classmethod
+    def login(cls, email, password):
+        
+        user= authenticate(
+            email= email,
+            password= password
+        )
+        
+        if not user:
+            return Response({"message": "Invalid credentials"}, status= status.HTTP_400_BAD_REQUEST)
+
+        refresh= RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+        
         
         
     @classmethod    
