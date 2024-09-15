f8e1cf5cde2f243f6c620fec7b052a895c17796f
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-15 14:56:16 -0300

    feat(api) Add Model client and serializers

--- api-server/app/serializers.py
+++ api-server/app/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
 
 class CsvDataSerializer(serializers.Serializer):
-    date = serializers.DateTimeField()
-    balance = serializers.FloatField()
-    equity = serializers.FloatField()
-    deposit = serializers.FloatField()
+    date = serializers.DateTimeField(required=True)
+    balance = serializers.FloatField(required=True)
+    equity = serializers.FloatField(required=True)
+    deposit = serializers.FloatField(required=True)
 
 class ImportDataSerializer(serializers.Serializer):
-    client_id = serializers.UUIDField()
-    import_type = serializers.IntegerField()
-    data = CsvDataSerializer(many=True)
+    client_id = serializers.UUIDField(required=True)
+    import_type = serializers.IntegerField(required=True)
+    data = CsvDataSerializer(many=True,required=True)
+    
+class ClientSerializer(serializers.Serializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only = True, required=True)
+    password_confirm = serializers.CharField(write_only = True, required = True)
