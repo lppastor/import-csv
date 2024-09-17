12c3d70f0849a08c0922ea9dab78068bde92c639
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-09-17 00:55:47 -0300

    feat(web): use "js-cookie" lib on axios instance

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { cookies } from 'next/headers'
+import Cookies from 'js-cookie'
 import { env } from '~/env'
 
 export const api = axios.create({
@@ -9,8 +9,7 @@ export const api = axios.create({
 // Request interceptor to add the token to headers
 api.interceptors.request.use(
   (config) => {
-    const cookieStore = cookies()
-    const token = cookieStore.get('token')
+    const token = Cookies.get('token')
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
@@ -19,21 +18,6 @@ api.interceptors.request.use(
     return config
   },
   (error) => {
-    return Promise.reject(error)
-  }
-)
-
-// Response interceptor to redirect to login if token is invalid
-api.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => {
-    if (error.response.status === 401) {
-      // redirect to login
-      window.location.href = '/login'
-    }
-
     return Promise.reject(error)
   }
 )
