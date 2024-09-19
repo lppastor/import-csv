95d6c39abc465b5d0825bccf0cbaee3b51b90e5c
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-18 23:01:22 -0300

    feat(api): Register route google_callback

--- api/app/urls.py
+++ api/app/urls.py
@@ -3,6 +3,7 @@ from .views.import_data import import_data, get_csv_data
 from .views.get_user_imports import get_user_imports
 from .views.graphdata import get_balance_summary
 from .views.Client import Create_client, ClientLogin, get_client
+from .views.google import google_login_callback
 
 urlpatterns = [
     path("import-data/", import_data, name="import_data"),
@@ -11,5 +12,6 @@ urlpatterns = [
     path('csv-data/', get_csv_data, name='csv-data'),
     path('client/register_client/', Create_client, name="Register Client"),
     path('client/login/', ClientLogin, name= "Login"),
-    path('client/me/', get_client, name= "Get Client")
+    path('client/me/', get_client, name= "Get Client"),
+    path('google_callback', google_login_callback, name='google callback')
 ]
