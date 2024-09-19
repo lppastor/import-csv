ac5885fa4882ee80662763a706bbfa0069caad85
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-19 12:35:10 -0300

    feat(api): Configs for autenticate google

--- api/project/settings.py
+++ api/project/settings.py
@@ -138,12 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Adicione isso
     ),
-    
-        'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    
 }
 
 SIMPLE_JWT = {
