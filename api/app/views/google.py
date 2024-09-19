e1570c1a8e7d52abc36a5d727a7e09b457979725
lucasmaciel.7 <lucasmacielcampos27@gami.com>
2024-09-19 12:34:20 -0300

    feat(api): configs autenticate for google

--- api/app/views/google.py
+++ api/app/views/google.py
@@ -2,17 +2,43 @@ import google.auth.transport.requests
 import google.oauth2.id_token
 from allauth.socialaccount.models import SocialAccount
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from django.shortcuts import redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+    
+    
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def google_login_callback(request):
+    # Verifica se o usuário está autenticado
+    if not request.user.is_authenticated:
+        return JsonResponse({"error": "Usuário não autenticado."}, status=401)
     
-    return JsonResponse({"status":"rota de redirect após autenticação"})
+    # Verifica se o campo client_id está presente
+    if not hasattr(request.user, 'client_id') or not request.user.client_id:
+        return JsonResponse({"error": "Client ID não encontrado para o usuário."}, status=400)
+
+    # Gera o JWT para o usuário autenticado
+    tokens = get_tokens_for_user(request.user)
+    access_token = tokens['access']
     
+    # URL do frontend
+    frontend_url = 'http://127.0.0.1:8000/app/swagger/'
+    redirect_url = f'{frontend_url}?token={access_token}'
+    
+    # Redireciona o usuário para o frontend com o token na URL
+    return redirect(redirect_url)
+
     
     
     
